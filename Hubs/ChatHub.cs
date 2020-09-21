2e8980a65033ca7e061d6de6fb6f8c7003eeb1a5
priottijp <jupapriotti@gmail.com>
2020-09-20 23:06:10 -0300

    First Commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatJuanPabloPriotti.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        public bool CheckLogin()
+         => this.Context.User.Identity.IsAuthenticated;
+
+        public string GetCurrentUser() => this.Context.User.Identity.Name;
+
+        public async Task ReadCSV(string stock_code)
+        {
+            Dictionary<string, string> stockData = new Dictionary<string, string>();
+            string path = $@"https://stooq.com/q/l/?s={stock_code.ToLower()}&f=sd2t2ohlcv&h&e=csv";
+
+            var response = new System.Net.WebClient().DownloadString(path).Split("\r\n");
+            var headers = response[0];
+            var headersValues = headers.Split(',');
+            var data = response[1];
+            var dataValues = data.Split(',');
+            for (int i = 0; i < headersValues.Count() - 1; i++)
+                stockData.Add(headersValues[i], dataValues[i]);
+            await Clients.All.SendAsync("ReceiveMessage", this.Context.User.Identity.Name, $"{stock_code.ToUpper()} quote is ${stockData.FirstOrDefault(k => k.Key == "Close").Value} per share");
+        }
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,58 @@
+﻿"use strict";
+
+var OnLoad = function () {
+    connection.invoke("CheckLogin")
+        .then(function (resp) {
+            if (resp) {
+                document.getElementById("divChat").style.display = "block";
+                connection.invoke("GetCurrentUser").then(function (user) {
+                    document.getElementById("userInput").value = user;
+                    document.getElementById("userInput").disabled = true;
+                })
+            }
+            else
+                document.getElementById("notLogged").style.display = "block";
+        })
+        .catch(function (err) {
+            return console.error(err.toString());
+        });
+    event.preventDefault();
+}
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says: " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    OnLoad();
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    debugger;
+    if (message.toString().substring(0, 7) === "/stock=") {
+        connection.invoke("ReadCSV", message.toString().substring(7)).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    else {
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+
+    event.preventDefault();
+});
